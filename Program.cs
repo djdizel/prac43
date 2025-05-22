134bb154395553b09bda447ba86b70fb1420e344
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-05-22 10:26:17 +0300

    123

--- Program.cs
+++ Program.cs
@@ -10,22 +10,7 @@ namespace prac43
     {
         static void Main(string[] args)
         {
-            MyClass myClass = new MyClass();
-            MyStruct myStruct = new MyStruct();
-            myClass.change = "не изменено";
-            myStruct.change = "не изменено";
-            ClassTaker(myClass);
-            StructTaker(myStruct);
-            Console.WriteLine(myClass.change);
-            Console.WriteLine(myStruct.change);
-        }
-        static void ClassTaker(MyClass myClass)
-        {
-            myClass.change = "изменено";
-        }
-        static void StructTaker(MyStruct myStruct)
-        {
-            myStruct.change = "изменено";
+            
         }
     }
 }
