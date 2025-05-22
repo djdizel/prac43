679e00fe0eafbfeec406b66d845e9e63ba3f690d
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-05-22 10:36:51 +0300

    end

---
+++ Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac43
+{
+    public struct Book
+    {
+        public string name;
+        public string author;
+        public int year;
+        public int pages;
+
+        public Book(string name, string author, int year, int pages)
+        {
+            this.name = name;
+            this.author = author;
+            this.year = year;
+            this.pages = pages;
+        }
+
+        public override string ToString()
+        {
+            return $"Название: {name}, Автор: {author}, Год издания: {year}, Колво страниц: {pages}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,21 @@ namespace prac43
     {
         static void Main(string[] args)
         {
-           
+            Book[] books = new Book[3];
+            books[0] = new Book("Война и мир", "Лев Толстой", 1869, 1225);
+            books[1] = new Book("1984", "Джордж Оруэлл", 1949, 328);
+            books[2] = new Book("Убить пересмешника", "Харпер Ли", 1960, 281);
+            for(int i =0; i<books.Length; i++)
+            {
+                Console.WriteLine("1-3 - информация о книгах \n0 - Выход");
+                int index = Convert.ToInt32(Console.ReadLine());
+                if (index == 0)
+                    break;
+                else if (index < 1 || index > 3)
+                    Console.WriteLine("Некорректный номер книги.");
+                else
+                    Console.WriteLine(books[index - 1].ToString());
+            }
         }
     }
 }
