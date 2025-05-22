4a9d1049978b0da09f3518cbc51bb4f37fcdd021
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-05-22 10:30:20 +0300

    num 2 end

---
+++ Notebook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac43
+{
+    public struct Notebook
+    {
+        public string model;
+        public string brand;
+        public double price;
+
+        public Notebook(string model, string brand, double price)
+        {
+            this.model = model;
+            this.brand = brand;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Модель: {model}, Производитель: {brand}, Цена: {price}";
+        }
+    }
+}
