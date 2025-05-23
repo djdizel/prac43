9ac4f337a4ad10accba3a5f3d9d6f8082c3a0cce
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-05-23 11:55:17 +0300

    123

--- Program.cs
+++ Program.cs
@@ -10,19 +10,34 @@ namespace prac43
     {
         static void Main(string[] args)
         {
+            //Экземпляр класса и структуры
             MyClass myClass = new MyClass();
             MyStruct myStruct = new MyStruct();
+            Console.WriteLine("До вызова методов");
+            //Вывод значений полей
             myClass.change = "не изменено";
             myStruct.change = "не изменено";
+            Console.WriteLine(myClass.change);
+            Console.WriteLine(myStruct.change);
+            Console.WriteLine("После вызова методов");
+            //Инициализация полей
             ClassTaker(myClass);
             StructTaker(myStruct);
             Console.WriteLine(myClass.change);
             Console.WriteLine(myStruct.change);
         }
+        /// <summary>
+        /// Метод, принимающий объект класса и принимает поле change
+        /// </summary>
+        /// <param name="myClass"></param>
         static void ClassTaker(MyClass myClass)
         {
             myClass.change = "изменено";
         }
+        /// <summary>
+        /// Метод, принимающий объект структуры и принимает поле change
+        /// </summary>
+        /// <param name="myStruct"></param>
         static void StructTaker(MyStruct myStruct)
         {
             myStruct.change = "изменено";
